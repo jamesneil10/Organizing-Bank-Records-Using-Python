6dbdf95d3d1a1f2891cee200b557f6bbf6ea503f
neil <jamesneil10@gmail.com>
2019-01-09 18:17:31 -0800

    push python hw

--- PyBank.py
+++ PyBank.py
@@ -1,1 +1,57 @@
+#import csv file
+import os
+import csv
 
+Mycsvfile = os.path.join('', 'Downloads', 'budget_data.csv')
+
+with open(Mycsvfile, 'r') as csvfile:
+
+
+    reader = csv.reader(csvfile, delimiter = ',')
+# Files to load (Remember to change these)
+
+    # use of next to skip first title row in csv file
+    next(reader) 
+    record_count = 0
+    netRev = 0
+    revenue = []
+    date = []
+    rev_change = []
+
+    # in this loop I did sum of column 1 which is revenue in csv file and counted total months which is column 0
+    
+
+    for row in reader:
+
+        revenue.append(int(row[1]))
+        date.append(row[0])
+
+
+    for row in reader:
+        record_count = record_count +1
+        if record_count > 0:
+            netRev = netRev + int(row[1])
+
+    print("Financial Analysis")
+    print("-----------------------------------")
+    print("Total Months:", len(date))
+    print("Total Revenue: $", sum(revenue))
+    #print("My way: ", netRev)
+
+
+    #in this loop I did total of difference between all row of column "Revenue" and found total revnue change. Also found out max revenue change and min revenue change. 
+    for i in range(1,len(revenue)):
+        rev_change.append(revenue[i] - revenue[i-1])   
+        avg_rev_change = sum(rev_change)/len(rev_change)
+
+        max_rev_change = max(rev_change)
+
+        min_rev_change = min(rev_change)
+
+        max_rev_change_date = str(date[rev_change.index(max(rev_change))])
+        min_rev_change_date = str(date[rev_change.index(min(rev_change))])
+
+
+    print("Average Revenue Change: $", round(avg_rev_change))
+    print("Greatest Increase in Revenue:", max_rev_change_date,"($", max_rev_change,")")
+    print("Greatest Decrease in Revenue:", min_rev_change_date,"($", min_rev_change,")")
--- PyPoll.py
+++ PyPoll.py
@@ -1,1 +1,51 @@
 
+
+#Create a python script to analyze polling results
+
+# import operating system & csv file
+import os
+import csv
+from collections import Counter
+
+# tell python where to find the csv file
+csvpath = os.path.join('', "Downloads", "election_data.csv")
+
+# open file
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+
+#have a place to store information
+    votes_count = Counter() 
+    candidates = [] 
+    percentage = []
+
+#get totals      
+    for row in csvreader: 
+        candidates.append(row[2])
+        total_votes = len(candidates)
+    
+#add votes to each candidate
+    for candidate in candidates: 
+        votes_count[candidate] += 1
+
+#connect the vote count to the candidate count
+    candidates = tuple(votes_count.keys())
+    votes = tuple(votes_count.values())
+
+#determine the winner
+    winner = max(zip(votes_count.values(), votes_count.keys()))
+
+#determine the percent votes
+    for x in votes:
+        percentage.append(round(int(x)/total_votes*100,2))
+
+#print the results
+    print("Election Results")
+    print("------------------------")
+    print ("total votes:", (total_votes))
+
+    print(list(zip(candidates, votes, percentage)))
+    print("------------------------")
+    print("Winner:",winner)
+    print("------------------------")
